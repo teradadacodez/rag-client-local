1371911b84afe4e1b980cf35c0cae2f81624d96f
Tanmay <tanmayjain10304@gmail.com>
2025-12-12 18:02:33 +0530

    version-1 too slow for now, updating it later :)

--- read_chunks.py
+++ read_chunks.py
@@ -1,5 +1,6 @@
 import requests
-
+import os
+import json
 
 def create_embedding(text) : 
     req = requests.post("http://localhost:11434/api/embeddings",
@@ -9,6 +10,21 @@ def create_embedding(text) :
 
     return embedding
 
+jsons = os.listdir("chunks/")
+print(jsons)
 
-temp = create_embedding("cat sat on the mat")
-print(temp)
+my_dicts = []
+chunk_id = 0
+for json_file in jsons :
+    file = None
+    with open(f"chunks/{json_file}","r",encoding="utf-8") as f:
+        file = json.load(f)
+    for chunk in file["chunks"] : 
+        chunk['chunk_id'] = chunk_id
+        chunk_embedding = create_embedding(chunk["text"])
+        chunk_id += 1
+        my_dicts.append(chunk)
+        print(chunk)
+    break 
+
+print(my_dicts)
