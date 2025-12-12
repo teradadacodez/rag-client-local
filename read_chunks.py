6791d17d25ea4c99d13d200e35655ffd5fe68631
Tanmay <tanmayjain10304@gmail.com>
2025-12-12 21:22:12 +0530

    version 3, created a embedding dataframe

--- read_chunks.py
+++ read_chunks.py
@@ -1,13 +1,15 @@
 import requests
 import os
 import json
+import pandas as pd
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 
 def create_embedding(text_list) : 
     req = requests.post("http://localhost:11434/api/embed",
-                        json={"model":"bge-m3","input":text_list})
-
+                        json={"model":"bge-m3",
+                              "input":text_list})
     embeddings = req.json()["embeddings"]
-
     return embeddings
 
 # a = create_embedding(["tanmay jain is a good boy","tanmay jain lives in alwar"])
@@ -23,13 +25,31 @@ for json_file in jsons :
     file = None
     with open(f"chunks/{json_file}","r",encoding="utf-8") as f:
         file = json.load(f)
+    print(f"Creating Embeddings for {json_file}")
     embeddings = create_embedding([c["text"] for c in file["chunks"]])
+
     for i,chunk in enumerate(file["chunks"]) : 
         chunk['chunk_id'] = chunk_id
         chunk['embedding'] = embeddings[i]
         chunk_id += 1
         my_dicts.append(chunk)
-        print(chunk)
-    break 
+    break
 
-print(my_dicts)
+
+df = pd.DataFrame.from_records(my_dicts)
+
+query = input("Ask a Question : ")
+query_embedding = create_embedding([query])[0]
+
+# print(np.vstack(df["embedding"].values))
+# print(np.vstack(df["embedding"]).shape)
+
+# finding similarities of query_embedding with chunk_embedding
+similarities = cosine_similarity(np.vstack(df["embedding"]),[query_embedding]).flatten()
+print(similarities)
+
+top_results = 5
+top_chunk_indices = similarities.argsort()[::-1][0:top_results]
+
+new_df = df.loc[top_chunk_indices]
+print(new_df[["number","text"]])
