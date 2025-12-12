7fb026f92407dc6d889c52f8c5fb1c67aca7bdb5
Tanmay <tanmayjain10304@gmail.com>
2025-12-12 18:45:37 +0530

    version 2, faster and reliable, changed path to localhost:11434/api/embed

--- read_chunks.py
+++ read_chunks.py
@@ -2,26 +2,31 @@ import requests
 import os
 import json
 
-def create_embedding(text) : 
-    req = requests.post("http://localhost:11434/api/embeddings",
-                        json={"model":"bge-m3","prompt":text})
+def create_embedding(text_list) : 
+    req = requests.post("http://localhost:11434/api/embed",
+                        json={"model":"bge-m3","input":text_list})
 
-    embedding = req.json()["embedding"]
+    embeddings = req.json()["embeddings"]
 
-    return embedding
+    return embeddings
 
-jsons = os.listdir("chunks/")
-print(jsons)
+# a = create_embedding(["tanmay jain is a good boy","tanmay jain lives in alwar"])
+# print(a[0],"\n\n\n\n",a[1])
+# print(type(a),type(a[0]),type(a[1]))
 
+
+jsons = os.listdir("chunks/") # listed all the jsons !
 my_dicts = []
 chunk_id = 0
+
 for json_file in jsons :
     file = None
     with open(f"chunks/{json_file}","r",encoding="utf-8") as f:
         file = json.load(f)
-    for chunk in file["chunks"] : 
+    embeddings = create_embedding([c["text"] for c in file["chunks"]])
+    for i,chunk in enumerate(file["chunks"]) : 
         chunk['chunk_id'] = chunk_id
-        chunk_embedding = create_embedding(chunk["text"])
+        chunk['embedding'] = embeddings[i]
         chunk_id += 1
         my_dicts.append(chunk)
         print(chunk)
